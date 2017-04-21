a12c7899df30fa2c9aec300115fc8fdfada32adc
Xairi <xairi.valdivia@gmail.com>
2017-04-21 10:02:52 -0500

    Update img_resize.py

--- img_resize.py
+++ img_resize.py
@@ -4,6 +4,43 @@ import os
 from PIL import Image
 
 
-img = Image.open("/home/prometeo/Python/resize_images/dados.png")
-img = img.convert('RGBA').convert('P', palette=Image.ADAPTIVE)
-img.save("/home/prometeo/Python/resize_images/dados-1.png", compress_level=9, bits=8)
+def compress_image(file, type=None):
+    filepath = os.path.join(os.getcwd(), file)
+    # filepath = os.path.join("/var/www/nginx/jockey/jockey_website/public/img/", file)
+    if type == 'png':
+        picture = Image.open(filepath)
+
+        if picture.mode =='RGBA':
+            # print('Es: RGBA')
+            picture = picture.convert('RGBA', palette=Image.WEB)
+            picture.save("/var/www/nginx/jockey_web/jockey_website/images/" + file, compress_level=7, bits=8)
+        if picture.mode == 'P':
+            # print('Es: P')
+            picture = picture.convert('RGBA').convert('P', palette=Image.ADAPTIVE)
+            picture.save("/var/www/nginx/jockey_web/jockey_website/images/" + file, compress_level=7, bits=8)
+
+
+    else:
+        picture = Image.open(filepath).convert('RGB')
+        picture.save("/var/www/nginx/jockey_web/jockey_website/images/" + file,
+                      "png", optimize=True, quality=75)
+
+
+def main():
+    # finds present working dir
+    pwd = os.getcwd()
+    #pwd = "/var/www/Jockey/jockey_website/public/img/"
+    num = 0
+
+    for file in os.listdir(pwd):
+        if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg',):
+            num += 1
+            compress_image(file)
+        if os.path.splitext(file)[1].lower() in ('.png',):
+            num += 1
+            compress_image(file, 'png')
+    print "Done"
+
+
+if __name__ == "__main__":
+    main()
